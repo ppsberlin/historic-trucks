619295482b2f26df372eba5aff9b81b459a1ffbd
ppsberlin <35074332+ppsberlin@users.noreply.github.com>
2018-01-04 18:15:30 +0100

    Add files via upload

---
+++ trucks/build.bat
@@ -0,0 +1,9 @@
+mads 2/no_name.asq -o:2man.xex > 2/!log.txt
+mads 3/no_name.asq -o:3skoda.xex > 3/!log.txt
+mads 4/no_name.asq -o:4s4000.xex > 4/!log.txt
+mads 5/no_name.asq -o:5scania.xex > 5/!log.txt
+mads 6/no_name.asq -o:6buesnag.xex > 6/!log.txt
+mads 7/no_name.asq -o:7man15215.xex > 7/!log.txt
+mads 8/no_name.asq -o:82henschel.xex > 8/!log.txt
+mads no_name.asq -o:trucks.xex > !log.txt
+start trucks.xex
